64c903e81d6adfcfe3439f12d6425d3d05f44bb1
Yuhang Yao <yh-yao@foxmail.com>
2021-05-08 14:22:03 -0400

    fix bug

--- nvidia-jetson-nano/fedavg/fedavg_jetson_nano_client.py
+++ nvidia-jetson-nano/fedavg/fedavg_jetson_nano_client.py
@@ -176,7 +176,7 @@ if __name__ == '__main__':
     model_trainer.set_id(client_index)
 
     # start training
-    trainer = FedAVGTrainer(client_index, train_data_local_dict, train_data_local_num_dict, train_data_num, device,
+    trainer = FedAVGTrainer(client_index, train_data_local_dict, train_data_local_num_dict, test_data_local_dict, train_data_num, device,
                             args, model_trainer)
 
     size = args.client_num_per_round + 1
